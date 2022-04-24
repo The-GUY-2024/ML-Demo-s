f432d200af047dffdbc4600aa5ac0c0958939342
Certified Humane <castrolwilliam@gmail.com>
2022-04-24 14:06:13 -0400

    Add files via upload

--- Math-stuff/mean.py
+++ Math-stuff/mean.py
@@ -3,7 +3,17 @@ num_list = [5,5,5,5]
 # This a basic arithmetic mean function 
 def mean(list):
     N = len(num_list)
+    adding = 0
     for t in range(len(num_list)):
-        print(num_list[t])
+
+        #add all the values in the list 
+        adding =  adding + int(num_list[t]) 
+#Get the arithmetic mean  
+    mean = adding / len(num_list);
+
+#Display the result
+    print("The arithmetic Mean: " + str(mean))
+    print("The sum of numbers: " + str(adding))
+    print("The total numbers: " + str(len(num_list)))
 
 mean(num_list)
