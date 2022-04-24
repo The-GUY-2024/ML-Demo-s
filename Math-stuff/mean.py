21d10d46e7b645475c9ec5281a7956a2d2f039f1
Certified Humane <castrolwilliam@gmail.com>
2022-04-24 13:37:06 -0400

    Add files via upload

--- Math-stuff/mean.py
+++ Math-stuff/mean.py
@@ -1,1 +1,9 @@
+num_list = [5,5,5,5]
 
+# This a basic arithmetic mean function 
+def mean(list):
+    N = len(num_list)
+    for t in range(len(num_list)):
+        print(num_list[t])
+
+mean(num_list)
